dbb580a03979d7756cf02212a8571bbcef8a38ee
hienqd <hienqd@rikkeisoft.com>
2023-11-22 13:46:42 +0700

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('cats/:id')
+  getHello(@Param('id') id: string): string {
+    console.log(id)
     return this.appService.getHello();
   }
 }
